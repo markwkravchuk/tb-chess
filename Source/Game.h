366a6bde231d88f1bcce75914bb47129af05fbe4
Mark Kravchuk <markkravchuk123@icloud.com>
2022-12-11 18:42:24 -0800

    Create game class with boilerplate

---
+++ Source/Game.h
@@ -0,0 +1,12 @@
+#ifndef CHESS_GAME_H
+#define CHESS_GAME_H
+
+namespace Chess {
+    class Game {
+        public:
+            Game();
+            void play();
+        private:
+    };
+}
+#endif
